4e2de2de28b718e233f970222abd46400dec9150
agrawal101 <gauravagrawal22499@gmail.com>
2026-05-23 23:12:32 +0530

    "test: add failing test for update employee"

--- backend/app/tests/test_employeeTest.py
+++ backend/app/tests/test_employeeTest.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 
 def test_create_employee():
     payload = {
-        "full_name": "John Doe",
+        "full_name": "Gaurav Agrawal",
         "job_title": "Software Engineer",
         "country": "India",
         "salary": 100000
@@ -22,7 +22,7 @@ def test_create_employee():
 
 def test_get_employee_by_id():
     payload = {
-        "full_name": "John Doe",
+        "full_name": "Gaurav Agrawal",
         "job_title": "Software Engineer",
         "country": "India",
         "salary": 100000
@@ -34,7 +34,7 @@ def test_get_employee_by_id():
     assert response.status_code == 200
     data = response.json()
     assert data["id"] == employee_id
-    assert data["full_name"] == "John Doe"
+    assert data["full_name"] == "Gaurav Agrawal"
 
 # def test_get_employee_by_invalid_id():
 #     response = client.get("/employees/999999")
@@ -44,7 +44,7 @@ def test_get_employee_by_id():
 def test_get_all_employees():
     employees = [
         {
-            "full_name": "John Doe",
+            "full_name": "Gaurav Agrawal",
             "job_title": "Software Engineer",
             "country": "India",
             "salary": 100000
@@ -73,7 +73,7 @@ def test_get_all_employees():
 def test_get_all_employees_with_pagination():
     employees = [
         {
-            "full_name": "John Doe",
+            "full_name": "Gaurav Agrawal",
             "job_title": "Software Engineer",
             "country": "India",
             "salary": 100000
@@ -106,3 +106,38 @@ def test_get_employees_invalid_size():
     response = client.get("/employees?page=1&size=0")
 
     assert response.status_code == 422
+
+def test_update_employee():
+    payload = {
+        "full_name": "Gaurav Agrawal",
+        "job_title": "Software Engineer",
+        "country": "India",
+        "salary": 100000
+    }
+
+    create_response = client.post(
+        "/employees",
+        json=payload
+    )
+
+    employee_id = create_response.json()["id"]
+
+    update_payload = {
+        "full_name": "Gaurav Updated",
+        "job_title": "Senior Engineer",
+        "country": "India",
+        "salary": 150000
+    }
+
+    response = client.put(
+        f"/employees/{employee_id}",
+        json=update_payload
+    )
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data["full_name"] == "Gaurav Updated"
+    assert data["job_title"] == "Senior Engineer"
+    assert data["salary"] == 150000
