9ac9ae48cd0b2081497722aac8cdf5778f1a6c65
agrawal101 <gauravagrawal22499@gmail.com>
2026-05-24 18:56:07 +0530

    test : added all the insight tests

--- backend/app/tests/test_employeeTest.py
+++ backend/app/tests/test_employeeTest.py
@@ -220,3 +220,67 @@ def test_get_salary_insights_by_country():
     assert data["minimum_salary"] == 50000
     assert data["maximum_salary"] == 200000
     assert data["average_salary"] == 116666.67
+
+def test_get_average_salary_by_job_title_in_country():
+    employees = [
+        {
+            "full_name": "Gaurav Agrawal",
+            "job_title": "Software Engineer",
+            "country": "India",
+            "salary": 100000
+        },
+        {
+            "full_name": "Ankit Jain",
+            "job_title": "Software Engineer",
+            "country": "India",
+            "salary": 200000
+        },
+        {
+            "full_name": "Rohit Sharma",
+            "job_title": "QA Engineer",
+            "country": "India",
+            "salary": 50000
+        }
+    ]
+
+    for employee in employees:
+        client.post("/employees", json=employee)
+
+    response = client.get(
+        "/salary-insights/job-title"
+        "?country=India"
+        "&job_title=Software Engineer"
+    )
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert data["country"] == "India"
+    assert data["job_title"] == "Software Engineer"
+    assert data["average_salary"] == 150000
+
+def test_salary_insights_country_not_found():
+    response = client.get(
+        "/salary-insights/country/Japan"
+    )
+
+    assert response.status_code == 404
+
+def test_job_title_salary_not_found():
+    employee = {
+        "full_name": "Gaurav Agrawal",
+        "job_title": "Software Engineer",
+        "country": "India",
+        "salary": 100000
+    }
+
+    client.post("/employees", json=employee)
+
+    response = client.get(
+        "/salary-insights/job-title"
+        "?country=India"
+        "&job_title=Doctor"
+    )
+
+    assert response.status_code == 404
